d8f35653bce7c5cc5a85c0e35bc1f537471a6325
Aqeel05 <syedaqeel5555@gmail.com>
2024-09-13 03:43:30 +0800

    hannahcafemenuwithcarousel

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ app.get('/', async (request, response) => {
     response.send( await readFile('./home.html', 'utf8') );
 });
 
-
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
 
 app.listen(process.env.PORT || 3000, () => console.log(`App available on http://localhost:3000`))
